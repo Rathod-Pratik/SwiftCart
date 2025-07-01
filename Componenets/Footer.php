16d1d16157e88768e5a62c7911f9aa52f73dca8a
Rathod Pratik <rathodpratik1928@gmail.com>
2025-07-01 10:06:20 +0530

    Implement code changes to enhance functionality and improve performance

--- Componenets/Footer.php
+++ Componenets/Footer.php
@@ -1,69 +1,68 @@
 <?php
 echo '
-
-
 <footer class="bg-white">
-    <div class="mx-auto w-full ">
+    <div class="mx-auto w-full">
+        <p class="py-5 border-t-[1px] border-b-[1px] border-gray-200 text-2xl font-semibold whitespace-nowrap text-center">SwiftCart</p>
       <div class="grid grid-cols-1  gap-8 px-4 py-6 lg:py-8 md:grid-cols-3">
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase ">About</h2>
-            <ul class="text-gray-500  font-medium">
+        <div class="mx-auto">
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase text-start">About</h2>
+            <ul class="text-gray-500 text-start font-medium">
                 <li class="mb-4">
-                    <a href="#" class=" hover:underline">About</a>
+                    <a href="#" class=" hover:underline">Our Company</a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Careers</a>
-                </li>
-                <li class="mb-4">
-                    <a href="#" class="hover:underline">Brand Center</a>
+                    <a href="#" class="hover:underline">Our Story</a>
                 </li>
                 <li class="mb-4">
                     <a href="#" class="hover:underline">Blog</a>
                 </li>
             </ul>
         </div>
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase">Information</h2>
-            <ul class="text-gray-500 font-medium">
+        <div class="m-auto">
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase text-start">Information</h2>
+            <ul class="text-gray-500 text-start font-medium">
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Discord Server</a>
+                    <a href="#" class="hover:underline">Delivery information</a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Twitter</a>
+                    <a href="#" class="hover:underline">Privacy policy</a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Facebook</a>
+                    <a href="#" class="hover:underline">Terms & Conditions</a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Contact Us</a>
+                    <a href="#" class="hover:underline">Return</a>
                 </li>
             </ul>
         </div>
-        <div>
-            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase ">Support</h2>
-            <ul class="text-gray-500  font-medium">
+        <div class="m-auto">
+            <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase text-start">Support</h2>
+            <ul class="text-gray-500 text-start  font-medium">
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Privacy Policy</a>
+                    <a href="#" class="hover:underline">Contact Us</a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Licensing</a>
+                    <a href="#" class="hover:underline">Help</a>
                 </li>
                 <li class="mb-4">
-                    <a href="#" class="hover:underline">Terms &amp; Conditions</a>
+                    <a href="#" class="hover:underline">FAQ</a>
+                </li>
+                <li class="mb-4">
+                    <a href="#" class="hover:underline">CheckOut</a>
                 </li>
             </ul>
         </div>
 
     </div>
     <div class="px-4 py-6 bg-gray-100  md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center">© 2023 <a href="https://flowbite.com/"></a>. All Rights Reserved.
+        <span class="text-sm text-gray-500 sm:text-center">© 2025 Shopizo. All Rights Reserved.
         </span>
         <div class="flex mt-4 sm:justify-center md:mt-0 space-x-5 rtl:space-x-reverse">
             <a href="#" class="text-gray-400 hover:text-gray-900 ">
                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
                         <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
                     </svg>
-                  <span class="sr-only">Facebook page</span>
+                  <span class="sr-only">Facebook</span>
               </a>
               <a href="#" class="text-gray-400 hover:text-gray-900 ">
                   <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
@@ -83,13 +82,9 @@ echo '
                   </svg>
                   <span class="sr-only">GitHub account</span>
               </a>
-              <a href="#" class="text-gray-400 hover:text-gray-900 ">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z" clip-rule="evenodd"/>
-                </svg>
-                  <span class="sr-only">Dribbble account</span>
-              </a>
         </div>
+            <img src="/SwiftCart/Image/Banking.png" alt="Banking" class="max-w-[150px] h-auto">
+        
       </div>
     </div>
 </footer>
--- Page/Auth/SignUp.php
+++ Page/Auth/SignUp.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SwiftCart | Login</title>
+    <title>SwiftCart | SignUp</title>
 </head>
 <body>
     <?php include '../../Componenets/Header.php'; ?>
